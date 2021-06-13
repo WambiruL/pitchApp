1f6b2808b9d602e11173d5738b59efdd53eb55a0
WambiruL <wambirulorraine@gmail.com>
2021-06-14 00:54:45 +0300

    Create pitch class to create new pitches

---
+++ app/models.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+from sqlalchemy.orm import backref
+from . import db
+
+class Pitch(db.Model):
+    __tablename__='pitch'
+
+    id=db.Column(db.Integer,primary_key=True)
+    pitch_title=db.Column(db.String)
+    pitch_content=db.Column(db.String(2000))
+    category=db.Column(db.String)
+    posted=db.Column(db.DateTime, default=datetime.utcnow)
+    comment=db.relationship('Comment', backref='pitch',lazy='dynamic') 
+    upvote = db.relationship('Upvote',backref='pitch',lazy='dynamic')
+    downvote = db.relationship('Downvote',backref='pitch',lazy='dynamic')
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
