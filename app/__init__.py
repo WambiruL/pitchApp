a495cf70a7dda687c1f8157d5822a0b5b73211a2
WambiruL <wambirulorraine@gmail.com>
2021-06-14 00:29:55 +0300

    Setting up configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,23 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from config import DevConfig
+from flask_sqlalchemy import SQLAlchemy
+
+bootstrap=Bootstrap()
+db=SQLAlchemy()
+
 #initializing application
 app=Flask(__name__)
 
+
+# Setting up configuration
+app.config.from_object(DevConfig)
+
 #initializing extensions
-bootstrap=Bootstrap(app)
+bootstrap.init_app(app)
+db.init_app(app)
+
+
+
 
 from app import views
