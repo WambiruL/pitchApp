16073e00db7b7cb09c82276fce33069c5462dfe0
WambiruL <wambirulorraine@gmail.com>
2021-06-14 00:34:36 +0300

    Import blueprint class

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,22 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
-from config import DevConfig
+from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 
 bootstrap=Bootstrap()
 db=SQLAlchemy()
 
 #initializing application
-app=Flask(__name__)
+def create_app(config_name):
+    app=Flask(__name__)
 
 
 # Setting up configuration
-app.config.from_object(DevConfig)
+    app.config.from_object(config_options[config_name])
 
 #initializing extensions
-bootstrap.init_app(app)
-db.init_app(app)
+    bootstrap.init_app(app)
+    db.init_app(app)
 
 
 
--- config.py
+++ config.py
@@ -24,3 +24,8 @@ class DevConfig(Config):
     '''
 
     DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
