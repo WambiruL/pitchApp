e41f17b74d2028ac3af93bd19ecc3f0adb6345cf
WambiruL <wambirulorraine@gmail.com>
2021-06-14 00:05:28 +0300

    Initialize boostrap extension

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
-
+from flask_bootstrap import Bootstrap
 #initializing application
 app=Flask(__name__)
+
+#initializing extensions
+bootstrap=Bootstrap(app)
 
 from app import views
