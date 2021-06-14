75692076d4d5dab7d225c09b08e8161783d94401
WambiruL <wambirulorraine@gmail.com>
2021-06-14 22:02:29 +0300

    Create a new_pitch view function

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,6 @@
+from app.main.forms import PitchForm
 from app.models import Pitch
-from flask import render_template
+from flask import render_template, url_for,redirect
 from . import main
 
 #views
@@ -27,3 +28,20 @@ def pitches():
     pitches=Pitch.query.all()
 
     return render_template('pitch.html',pitches=pitches)
+
+@main.route('/new_pitch', methods=['GET', 'POST'])
+def new_pitch():
+    pitch_form=PitchForm()
+    if pitch_form.validate_on_submit():
+        title=pitch_form.title.data
+        pitch=pitch_form.text.data
+        category=pitch_form.category.data
+
+        new_pitch=Pitch(pitch_title=title, pitch_content=pitch, category=category)
+
+        new_pitch.save_pitch()
+        return redirect(url_for('.index'))
+
+    title='New Pitch'
+
+    return render_template('new_pitch.html', title=title, pitch_form=pitch_form)
