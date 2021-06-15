c3f60e06beda5e61a04383a8355022cf52d7b295
WambiruL <wambirulorraine@gmail.com>
2021-06-15 10:48:06 +0300

    Add the logout user view function

--- app/auth/views.py
+++ app/auth/views.py
@@ -3,7 +3,7 @@ from flask import render_template, redirect,url_for, flash, request
 from . import auth
 from ..import db
 from ..models import User
-from flask_login import login_user
+from flask_login import login_user, logout_user,login_required
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
@@ -23,9 +23,15 @@ def login():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        user = User(email=form.email.data, username=form.username.data,password=form.password.data)
         db.session.add(user)
         db.session.commit()
         return redirect(url_for('auth.login'))
         title = "New Account"
     return render_template('auth/register.html',registration_form = form)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
