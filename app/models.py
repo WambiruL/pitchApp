6f1426187d12fee13e84e946fb7fee87d10a3203
WambiruL <wambirulorraine@gmail.com>
2021-06-15 10:30:14 +0300

    Pass user id to load user to get user

--- app/models.py
+++ app/models.py
@@ -1,8 +1,13 @@
 from datetime import datetime
-from enum import unique
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
+from . import login_manager
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 
 class Pitch(db.Model):
     __tablename__='pitch'
