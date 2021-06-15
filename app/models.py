9ea683af7073a0c0d0db23eee58c5adb03bcfc38
WambiruL <wambirulorraine@gmail.com>
2021-06-15 10:28:45 +0300

    Add email and password hash columns

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 from datetime import datetime
+from enum import unique
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
 
 class Pitch(db.Model):
     __tablename__='pitch'
@@ -21,11 +23,13 @@ class Pitch(db.Model):
     def __repr__(self):
         return f'Pitch {self.post}'
         
-class User(db.Model):
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
+    email=db.Column(db.String(255), unique=True, index=True)
     username = db.Column(db.String(255))
     pass_secure=db.Column(db.String(255))
+    password_hash=db.Column(db.String(255))
 
     @property
     def password(self):
