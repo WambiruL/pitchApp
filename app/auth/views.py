0f7ff6458ce7a4e1ec0df4b8483e61ec59065d3f
WambiruL <wambirulorraine@gmail.com>
2021-06-15 10:43:17 +0300

    Create loginform view function

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,12 +1,23 @@
-from .forms import RegistrationForm
-from flask import render_template, redirect,url_for
+from .forms import RegistrationForm, LoginForm
+from flask import render_template, redirect,url_for, flash, request
 from . import auth
 from ..import db
 from ..models import User
+from flask_login import login_user
 
-@auth.route('/login')
+@auth.route('/login',methods=['GET','POST'])
 def login():
-    return render_template('auth/login.html')
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None:
+            login_user(user,login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+
+        flash('Invalid username or Password')
+
+    title = "Pitch Login"
+    return render_template('auth/login.html',login_form = login_form,title=title)
 
 @auth.route('/register',methods = ["GET","POST"])
 def register():
