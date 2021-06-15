89d1412c47ff27f2c60ce98717ac968de34f8880
WambiruL <wambirulorraine@gmail.com>
2021-06-16 01:44:16 +0300

    Add profilepic path and bio to users

--- app/models.py
+++ app/models.py
@@ -37,6 +37,8 @@ class User(UserMixin,db.Model):
     username = db.Column(db.String(255))
     pass_secure=db.Column(db.String(255))
     password_hash=db.Column(db.String(255))
+    bio=db.Column(db.String(255))
+    profile_pic_path=db.Column(db.String())
 
     @property
     def password(self):
