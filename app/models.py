230da9bca6f444d212da875e7385449e8261bcf5
WambiruL <wambirulorraine@gmail.com>
2021-06-15 10:05:38 +0300

    Add class user

--- app/models.py
+++ app/models.py
@@ -19,3 +19,13 @@ class Pitch(db.Model):
         
     def __repr__(self):
         return f'Pitch {self.post}'
+        
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+    pass_secure=db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'User {self.username}'
+       
