528681ea3d39fd412250a5d2bd88ac074a2f5254
WambiruL <wambirulorraine@gmail.com>
2021-06-16 02:32:27 +0300

    Include upload photos view function

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,9 @@
 from app.main.forms import CommentForm, PitchForm,UpdateProfile
 from app.models import Comment, Pitch, User
-from flask import render_template, url_for,redirect,abort
+from flask import render_template, url_for,redirect,abort,request
 from flask_login import login_required,current_user
 from . import main
-from .. import db
+from .. import db,images
 
 #views
 @main.route('/')
@@ -99,4 +99,15 @@ def update_profile(uname):
         return redirect(url_for('.profile',uname=user.username))
 
     return render_template('profile/update.html',form =form)
+
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = images.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
 
