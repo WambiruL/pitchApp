e88c6a9209c15b1cdad761a38c18b0af5bee97cc
WambiruL <wambirulorraine@gmail.com>
2021-06-16 20:39:07 +0300

    Add email messages to the user

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,3 +1,4 @@
+from app.email import mail_message
 from .forms import RegistrationForm, LoginForm
 from flask import render_template, redirect,url_for, flash, request
 from . import auth
@@ -26,6 +27,7 @@ def register():
         user = User(email=form.email.data, username=form.username.data,password=form.password.data)
         db.session.add(user)
         db.session.commit()
+        mail_message("Welcome to PITCHAPP","email/welcome_user",user.email,user=user)
         return redirect(url_for('auth.login'))
     title = "New Account"
     return render_template('auth/register.html',registration_form = form, title=title)
---
+++ start.sh
@@ -0,0 +1,3 @@
+export MAIL_USERNAME=lorraine.wambui@student.moringaschool.com
+export MAIL_PASSWORD=wambui2002
+python3.6 manage.py server
